bdfc704d5049208821034cbdbd1bbb4bb86db392
Claes Hallström <hallstrom.claes@gmail.com>
2019-08-17 08:38:35 +0200

    Move constants to const.py

---
+++ pysystembolaget/const.py
@@ -0,0 +1,3 @@
+"""Systembolaget constants."""
+_API_BASE_URL = 'https://api-extern.systembolaget.se/product/v1/'
+API_PRODUCT_URL = _API_BASE_URL + 'product/'
--- pysystembolaget/product.py
+++ pysystembolaget/product.py
@@ -1,29 +1,28 @@
 """Systembolaget Product API."""
 from . import session
+from .const import API_PRODUCT_URL
 
 
 class Product():
     """Product API."""
 
-    BASE_URL = 'https://api-extern.systembolaget.se/product/v1/product/'
-
     @classmethod
     def get(cls, product_id):
         """Get."""
-        url = cls.BASE_URL + str(product_id)
+        url = API_PRODUCT_URL + str(product_id)
         response = session.get(url)
         return response.json()
 
     @classmethod
     def get_all_products(cls):
         """Get all products."""
-        url = cls.BASE_URL
+        url = API_PRODUCT_URL
         response = session.get(url)
         return response.json()
 
     @classmethod
     def get_products_with_store(cls):
         """Get products with store."""
-        url = cls.BASE_URL + 'getproductswithstore'
+        url = API_PRODUCT_URL + 'getproductswithstore'
         response = session.get(url)
         return response.json()
