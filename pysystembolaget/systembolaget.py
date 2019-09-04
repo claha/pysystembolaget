99afe293893cef13b12785ad4bb0c74f03128ce3
Claes Hallström <hallstrom.claes@gmail.com>
2019-09-04 21:16:48 +0200

    Simplyify product search function

--- pysystembolaget/systembolaget.py
+++ pysystembolaget/systembolaget.py
@@ -56,56 +56,33 @@ class Systembolaget():
                        sell_start_date_from=None, sell_start_date_to=None):
         """API: Product_Search."""
         params = {}
-        if sort_direction:
-            params['SortDirection'] = sort_direction
-        if sort_by:
-            params['SortBy'] = sort_by
-        if page:
-            params['Page'] = page
-        if search_query:
-            params['SearchQuery'] = search_query
-        if country:
-            params['Country'] = country
-        if origin_level_1:
-            params['OriginLevel1'] = origin_level_1
-        if origin_level_2:
-            params['OriginLevel2'] = origin_level_2
-        if origin_level_3:
-            params['OriginLevel3'] = origin_level_3
-        if vintage:
-            params['Vintage'] = vintage
-        if sub_category:
-            params['SubCategory'] = sub_category
-        if type:
-            params['Type'] = type
-        if style:
-            params['Style'] = style
-        if assortment_text:
-            params['AssortmentText'] = assortment_text
-        if bottle_type_group:
-            params['BottleTypeGroup'] = bottle_type_group
-        if seal:
-            params['Seal'] = seal
-        if grape:
-            params['Grape'] = grape
-        if price_min:
-            params['PriceMin'] = price_min
-        if price_max:
-            params['PriceMax'] = price_max
-        if alcohol_percentage_min:
-            params['AlcoholPercentageMin'] = alcohol_percentage_min
-        if alcohol_percentage_max:
-            params['AlcoholPercentageMax'] = alcohol_percentage_max
-        if news:
-            params['News'] = news
-        if csr:
-            params['Csr'] = csr
-        if other_selections:
-            params['OtherSelections'] = other_selections
-        if sell_start_date_from:
-            params['SellStartDateFrom'] = sell_start_date_from
-        if sell_start_date_to:
-            params['SellStartDateTo'] = sell_start_date_to
+        params['SortDirection'] = sort_direction
+        params['SortBy'] = sort_by
+        params['Page'] = page
+        params['SearchQuery'] = search_query
+        params['Country'] = country
+        params['OriginLevel1'] = origin_level_1
+        params['OriginLevel2'] = origin_level_2
+        params['OriginLevel3'] = origin_level_3
+        params['Vintage'] = vintage
+        params['SubCategory'] = sub_category
+        params['Type'] = type
+        params['Style'] = style
+        params['AssortmentText'] = assortment_text
+        params['BottleTypeGroup'] = bottle_type_group
+        params['Seal'] = seal
+        params['Grape'] = grape
+        params['PriceMin'] = price_min
+        params['PriceMax'] = price_max
+        params['AlcoholPercentageMin'] = alcohol_percentage_min
+        params['AlcoholPercentageMax'] = alcohol_percentage_max
+        params['News'] = news
+        params['Csr'] = csr
+        params['OtherSelections'] = other_selections
+        params['SellStartDateFrom'] = sell_start_date_from
+        params['SellStartDateTo'] = sell_start_date_to
+        params = {key: value for key, value in params.items()
+                  if value is not None}
         params = urllib.parse.urlencode(params)
         url = API_PRODUCT_URL + 'search?%s' % params
         return self.__GET__(url)
